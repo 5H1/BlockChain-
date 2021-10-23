05f8ceb6e1e3bd7073b4854d22ea3b53aad38d85
Vallepu Vamsi Krishna <vallep670@gmail.com>
2021-10-23 15:14:32 +0530

    added deploy.js

--- deploy.js
+++ deploy.js
@@ -1,3 +1,18 @@
-const HDWalletProvider = require('truffle-hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
 const {interface, bytecode} = require('./compile');
+
+const provider = new HDWalletProvider(
+    'barrel wise home outdoor amused enrich lunch then envelope round attract core',
+    'https://rinkeby.infura.io/v3/cdd1fe4044e44c9a8fb2fc181c5d17b0'
+);  //mnemonic, api
+
+const web3 = new Web3(provider);
+const deploy = async()=>{
+    const accounts = await web3.eth.getAccounts();
+    console.log("deploying contract from account : ",accounts[0]);
+
+    const result = await new web3.eth.Contract(interface).deploy({data: bytecode, arguments:[5]}).send({gas:'1000000', from: accounts[0]});    
+    console.log("Contract deployed to : ", result.options.address);
+};
+deploy();
